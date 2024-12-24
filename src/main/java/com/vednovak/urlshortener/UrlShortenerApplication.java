be0450f261c65fda3549b9f5a1bab7442b0fdd10
Vedran Novak <9297079+NovakVed@users.noreply.github.com>
2024-12-24 02:02:36 +0100

    add todos

--- src/main/java/com/vednovak/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/vednovak/urlshortener/UrlShortenerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UrlShortenerApplication {
 
     // TODO: write unit tests / integration tests
+    // https://stackoverflow.com/questions/64193818/spring-security-default-password-is-not-printing-on-console
+    // https://stackoverflow.com/questions/37285016/what-is-username-and-password-when-starting-spring-boot-with-tomcat
     // TODO: add actuator for monitoring - make it work
     // TODO: migrate to postgreSQL
     // TODO: add caching
